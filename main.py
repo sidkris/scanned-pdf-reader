711f09237751455940c6cefb99b3e40bf834252a
Siddharth Krishnan <sidkrishnan@protonmail.com>
2024-01-22 23:26:22 +0530

    updates openai model

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAIS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 import os
+from dotenv import load_dotenv
 
-os.environ['OPENAI_API_KEY'] = "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"
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # extract data from the PDF
 reader = PdfReader(r'bell_labs.pdf')
@@ -22,14 +25,14 @@ for i, page in enumerate(reader.pages):
     raw_text += text
 
 # Split the text into sentences / smaller chunks
-splitter = CharacterTextSplitter(separator = "\n", chunk_size = 1250, chunk_overlap = 200, length_function = len)
+splitter = CharacterTextSplitter(separator = "\n", chunk_size = 2000, chunk_overlap = 200, length_function = len)
 sentences = splitter.split_text(raw_text)
 
 # Text Embeddings
 embeddings = OpenAIEmbeddings()
 
 doc_search = FAISS.from_texts(sentences, embeddings)
-chain = load_qa_chain(OpenAI(), chain_type = 'stuff')
+chain = load_qa_chain(OpenAI(model = 'gpt-3.5-turbo-instruct'), chain_type = 'stuff')
 
 while True:
     
